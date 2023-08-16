848415a570944b349108e909f6cc57bd01edbb45
Basil Kohler <basil.kohler@one-inside.com>
2023-08-16 09:40:14 +0200

    fixed navigate word wrap

--- init.lua
+++ init.lua
@@ -237,8 +237,8 @@ function map(mode, lhs, rhs, desc, opts)
 end
 
 -- Remap for dealing with word wrap
-map('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
-map('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
 
 -- Highlight on yank
